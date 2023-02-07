58cbe6af393fc700b19a287e3432b2262d1ab2b8
DaniSald <dmss@icomp.ufam.edu.br>
2023-02-07 11:29:30 -0400

    ajeitando um pouco a bagunça

--- app.js
+++ app.js
@@ -7,38 +7,21 @@ const app = express();
 
 const handlebars = require('express-handlebars');
 const { search } = require("./config/routes");
-/*app.get('/results', req, res) =>{
-  const searchTerm = req.query.q;
-  res.render('results', {})
-}
-*/
+
 app.engine("handlebars", handlebars.engine({
-    layoutsDir: `${__dirname}/views`,
-    defaultLayout: 'main',
+  layoutsDir: `${__dirname}/views`,
+  defaultLayout: 'main',
 }));
 app.set("view engine", "handlebars");
 app.set("views", `${__dirname}/views`);
 
-/*app.get("/search/:query", async (req, res) => {
-    try {
-      const library = await fs.promises.readFile("library.json", "utf-8");
-      const documents = JSON.parse(library);
-      const results = documents.filter(
-        document => document.title.includes(req.params.query)
-      );
-      res.json(results);
-    } catch (error) {
-      res.status(500).send({ error: "Não encontrou a biblioteca" });
-    }
-  }); */
 
-
-  app.get('/json-to-html', (req, res)=>{
-    const jsonData = [
-      {filename:''}
-    ];
-    res.render('json-to-html', {data: jsonData})
-  })
+app.get('/json-to-html', (req, res) => {
+  const jsonData = [
+    { filename: '' }
+  ];
+  res.render('json-to-html', { data: jsonData })
+})
 
 
 app.use(express.urlencoded({ extended: false }));
@@ -47,22 +30,22 @@ app.use(express.json());
 app.use('/', router);
 
 
-app.get('/', function(req, res){
-    res.render('index')
-});    
+app.get('/', function (req, res) {
+  res.render('index')
+});
 
 app.use(sass({
-    src: `${__dirname}/public/scss`,
-    dest: `${__dirname}/public/css`,
-    outputStyle: "compressed",
-    prefix: "/css"
+  src: `${__dirname}/public/scss`,
+  dest: `${__dirname}/public/css`,
+  outputStyle: "compressed",
+  prefix: "/css"
 }));
 
 app.use("/css", express.static(`${__dirname}/public/css`));
 
 app.use(router);
 app.listen(4444, () => {
-console.log("Express app iniciada na porta 4444.");
+  console.log("Express app iniciada na porta 4444.");
 });
 
 
--- config/routes.js
+++ config/routes.js
@@ -7,11 +7,8 @@ const axios = require('axios');
 router.get('/results/:id', async (req, res) => {
   try {
     const { id } = req.params
-    console.log(id)
-    const response = await axios.get(`http://localhost:7890/doc/${id}`);
-    //const json = JSON.parse(response.data)
-    console.log('deu certo', response.data)
-    res.render('results', { data:response.data._source.text, filename:response.data._source.filename   });
+    const response = await axios.get(`http://localhost:7890/doc/${id}`)
+    res.render('results', { data: response.data._source.text, filename: response.data._source.filename });
 
   } catch (error) {
     console.log(error)
