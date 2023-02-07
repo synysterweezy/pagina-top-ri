0b922d567e9f7ce2f852237e78f89e697e0150fe
Claver <edward.claver@icomp.ufam.edu.br>
2023-02-07 00:00:55 -0400

    finalizando os trabalhos

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const router = require("./config/routes");
 const sass = require('node-sass-middleware');
-
+//const fs = require("fs");
 
 const app = express();
 
 const handlebars = require('express-handlebars');
 const { search } = require("./config/routes");
-
+/*app.get('/results', req, res) =>{
+  const searchTerm = req.query.q;
+  res.render('results', {})
+}
+*/
 app.engine("handlebars", handlebars.engine({
     layoutsDir: `${__dirname}/views`,
     defaultLayout: 'main',
@@ -15,6 +19,32 @@ app.engine("handlebars", handlebars.engine({
 app.set("view engine", "handlebars");
 app.set("views", `${__dirname}/views`);
 
+/*app.get("/search/:query", async (req, res) => {
+    try {
+      const library = await fs.promises.readFile("library.json", "utf-8");
+      const documents = JSON.parse(library);
+      const results = documents.filter(
+        document => document.title.includes(req.params.query)
+      );
+      res.json(results);
+    } catch (error) {
+      res.status(500).send({ error: "Não encontrou a biblioteca" });
+    }
+  }); */
+
+
+  app.get('/json-to-html', (req, res)=>{
+    const jsonData = [
+      {filename:''}
+    ];
+    res.render('json-to-html', {data: jsonData})
+  })
+
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use('/', router);
 
 
 app.get('/', function(req, res){
@@ -31,8 +61,8 @@ app.use(sass({
 app.use("/css", express.static(`${__dirname}/public/css`));
 
 app.use(router);
-app.listen(3000, () => {
-console.log("Express app iniciada na porta 3000.");
+app.listen(4444, () => {
+console.log("Express app iniciada na porta 4444.");
 });
 
 
--- config/routes.js
+++ config/routes.js
@@ -4,17 +4,19 @@ const axios = require('axios');
 
 
 
+router.get('/results/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    console.log(id)
+    const response = await axios.get(`http://localhost:7890/doc/${id}`);
+    //const json = JSON.parse(response.data)
+    console.log('deu certo', response.data)
+    res.render('results', { data:response.data._source.text, filename:response.data._source.filename   });
 
-router.get('/search', (req, res) => {
-    const searchTerm = req.query.q;
-  
-    axios.get(`https://api.github.com/search/users?q=${searchTerm}`)
-      .then(apiResponse => {
-        res.send(apiResponse.data);
-      })
-      .catch(error => {
-        res.send('Erro: ' + error);
-      });
-  });
+  } catch (error) {
+    console.log(error)
+    res.render('results', { error: 'Erro ao buscar na biblioteca de documentos' });
+  }
+});
 
-  module.exports = router;
+module.exports = router;
